09081bccc588babc3dd624b0ebca58e0bc579e42
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2024-02-13 13:47:54 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio1DeRepaso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un nuemero de minutos");
+            int numeroUsu=Convert.ToInt32(Console.ReadLine());
+            int numeroSegundos;
+            int numeroMinutos;
+            int numeroHoras;
+            int numeroDias;
+
+            numeroSegundos=numeroUsu*60;
+            numeroMinutos = numeroUsu;
+            numeroHoras = numeroMinutos / 60;
+            numeroDias = numeroMinutos /24;
+
+            Console.WriteLine(numeroUsu + "con: " +numeroDias+" "+"dias"+" "+ numeroHoras+" "+"horas"+"  "+numeroMinutos+" "+"minutos" );
+        }
+    }
+}
